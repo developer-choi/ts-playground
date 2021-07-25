a160f5ac0d1e2516fdd4975654548617c5bb689f
choeyujin <dbwls6840@naver.com>
2021-07-26 08:30:16 +0900

    Add add() and toArray() and remove() of LinkedList.

--- index.ts
+++ index.ts
@@ -1,55 +1,10 @@
-class Node<T> {
-  data: T;
-  next: Node<T> | null;
-  
-  constructor(data: T, next: Node<T> | null = null) {
-    this.data = data;
-    this.next = next;
-  }
-}
-
-export class LinkedList<T> {
-  private head: Node<T> | null;
-  private last: Node<T> | null;
-  length: number;
-  
-  constructor() {
-    this.head = null;
-    this.last = null;
-    this.length = 0;
-  }
-  
-  add(item: T) {
-    const newNode = new Node(item);
-  
-    if (!this.head) {
-      this.head = newNode;
-      this.last = newNode;
-      
-    } else if(this.last) {
-      this.last.next = newNode;
-      this.last = newNode;
-    }
-    
-    this.length++;
-  }
-  
-  toArray() {
-    const array: T[] = [];
-    let node = this.head;
-  
-    while (node) {
-      array.push(node.data);
-      node = node.next;
-    }
-  
-    return array;
-  }
-}
+import {LinkedList} from './src/data-structure/list/LinkedList';
 
 const list = new LinkedList<number>();
 list.add(1);
 list.add(2);
 list.add(3);
-console.log(list.toArray());
-console.log(list.length);
+console.log(list.remove(4), list.toArray());
+console.log(list.remove(3), list.toArray());
+console.log(list.remove(2), list.toArray());
+console.log(list.remove(1), list.toArray());
