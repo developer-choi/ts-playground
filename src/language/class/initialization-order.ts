138a5ad19761f87ea44db3c1b5c65b7b98d6babd
choeyujin <dbwls6840@naver.com>
2021-07-26 07:19:03 +0900

    Add a class initialization order example.

---
+++ src/language/class/initialization-order.ts
@@ -0,0 +1,26 @@
+function logAndGetString(value: string) {
+  console.log('return', value);
+  return value;
+}
+
+class Base {
+  constructor() {
+    this.name = logAndGetString('base-name in constructor');
+  }
+  
+  name = logAndGetString('base-name');
+}
+
+class DerivedClass extends Base {
+  // name = logAndGetString('derived-name');
+  
+  constructor() {
+    super();
+    this.name = logAndGetString('derived-name in constructor');
+  }
+  
+  name = logAndGetString('derived-name');
+}
+
+const derivedInstance = new DerivedClass();
+console.log(derivedInstance);
