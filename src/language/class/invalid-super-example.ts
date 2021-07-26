d3ac9c79eaf32f15ec72594231b6074534b300e5
choeyujin <dbwls6840@naver.com>
2021-07-27 08:25:38 +0900

    Add a comment.

--- src/language/class/invalid-super-example.ts
+++ src/language/class/invalid-super-example.ts
@@ -14,6 +14,7 @@ class Derived extends Base {
      * TS2340: Only public and protected methods of the base class are accessible via the 'super' keyword.
      * https://stackoverflow.com/a/51833890
      * https://github.com/Microsoft/TypeScript/issues/338
+     * I don't know what's the difference super.baseField vs this.baseField.
      */
     console.log(super.baseField);
   }
